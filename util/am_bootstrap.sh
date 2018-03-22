ca45b54de27743baa964b477afa676b5f1a95efa
Warren Strange <warren.strange@forgerock.com>
2018-03-21 19:32:35 -0600

    make  basedn an env var

--- util/am_bootstrap.sh
+++ util/am_bootstrap.sh
@@ -4,6 +4,8 @@
 # TODO: Deprecate this when we get boot from json support
 set -x
 
+BASE_DN="${BASE_DN:-dc=openam,dc=forgerock,dc=org}"
+
 # Configuration store LDAP. Defaults to the configuration store stateful set running in the same namespace.
 CONFIGURATION_LDAP="${CONFIGURATION_LDAP:-configstore-0.configstore:1389}"
 
@@ -15,7 +17,7 @@ OPENAM_HOME=${OPENAM_HOME:-/home/forgerock/openam}
 # Test the configstore to see if it contains a configuration. Return 0 if configured.
 is_configured() {
     echo "Testing if the configuration store is configured with an AM installation"
-    test="ou=services,dc=openam,dc=forgerock,dc=org"
+    test="ou=services,$BASE_DN"
     r=`ldapsearch -y ${DIR_MANAGER_PW_FILE} -A -H "ldap://${CONFIGURATION_LDAP}" -D "cn=Directory Manager" -s base -l 5 -b "$test"  > /dev/null 2>&1`
     status=$?
     echo "Is configured exit status is $status"
